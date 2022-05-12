f492eadca05194da7e3b295dcebbedb5bc1f99be
Naftali Kulik <nkulik1994@gmail.com>
2022-05-12 14:43:43 -0400

    completed lab requirements including bonus

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
+let currentUser
 document.addEventListener("DOMContentLoaded", function() {
+    fetch('http://localhost:3000/users')
+    .then(res => res.json())
+    .then(users => logIn(users))
     fetch('http://localhost:3000/books')
     .then(res => res.json())
     .then(bookList => bookList.map(book => renderBookList(book)))
 });
+
+function logIn(users) {
+    return currentUser = users[Math.floor(Math.random() * users.length)]
+}
 
 function renderBookList(book) {
     const li = document.createElement('li')
@@ -23,13 +31,51 @@ function renderBookInfo(book) {
         <p>Liked by:</p>
             <ul id="like-list">
             </ul>
-        <button>Like</button
+        <button id="like-${book.id}"></button
     `
     document.getElementById('show-panel').appendChild(details)
     function listLike(user) {
         const li = document.createElement('li')
         li.textContent = user.username
+        li.className = 'like-list'
         document.getElementById('like-list').appendChild(li)
     }
+    const button = document.getElementById(`like-${book.id}`)
     book.users.map(user => listLike(user))
+    book.users.find(user => user.username === currentUser.username) === undefined ? button.textContent = 'Like' : button.textContent = 'Unlike'
+    button.addEventListener('click', () => {
+        button.textContent === 'Like' ? likeBook(book) : unLikeBook(book)
+    })
+}
+
+function likeBook(book) {
+    book.users.push(currentUser)
+    handlePatch(book)
+}
+
+function unLikeBook(book) {
+    const length = book.users.length
+    for (let i = 0; i < length; i++) {
+        if (book.users[i].username === currentUser.username) {
+            book.users.splice(i, 1)
+        }
+    }
+    handlePatch(book)
+}
+
+function handlePatch(book) {
+    const body = {
+        users: book.users
+    }
+    const configObj = {
+        method: 'PATCH',
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(body)
+    }
+    fetch(`http://localhost:3000/books/${book.id}`, configObj)
+    .then(res => res.json())
+    .then(() => renderBookInfo(book))
 }
